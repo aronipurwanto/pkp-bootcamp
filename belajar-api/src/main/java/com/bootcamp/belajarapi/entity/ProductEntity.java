9ab16ee25d4abe31085983647da3d9e0899cdf0f
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-10-23 14:28:47 +0700

    feat: CRUD Product

--- belajar-api/src/main/java/com/bootcamp/belajarapi/entity/ProductEntity.java
+++ belajar-api/src/main/java/com/bootcamp/belajarapi/entity/ProductEntity.java
@@ -13,7 +13,15 @@ import lombok.NoArgsConstructor;
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
     private int id;
+
+    @Column(name = "product_name", length = 120)
     private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "product_price")
     private Long price;
 }
